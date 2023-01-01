721a3e668818497859f5a5e82f0a7c1970fe8b50
Marta Białecka <marta.bialecka@outlook.com>
2023-01-02 00:36:07 +0100

    Iterate through the dictionary, add captions and capitalize text

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,7 @@ lista_zakupów = {
 "piekarnia" : ["chleb", "pączek", "bułki"],
 "warzywniak" : ["marchew", "seler", "rukola"]
 }
+print ("Lista zakupów")
+for i in lista_zakupów.items():
+    print (f"Idę do {i[0].capitalize()}. Kupuję tu następujące rzeczy: {[towar.capitalize() for towar in i[1]]}")
+    
