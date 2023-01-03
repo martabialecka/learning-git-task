f0b0b420116f54c01cec51c69baabc8d87024fec
Marta Białecka <marta.bialecka@outlook.com>
2023-01-03 14:48:49 +0100

    Add the shopping satisfaction

--- shopping_list.py
+++ shopping_list.py
@@ -8,4 +8,5 @@ for i in lista_zakupów.items():
     print (f"Idę do {i[0].capitalize()}. Kupuję tu następujące rzeczy: {[towar.capitalize() for towar in i[1]]}")
     s = s + len (i[1])
 print (f"W sumie kupuję {s} produktów.")
+print ("Jestem zadowolona z takich zakupów.")
     
